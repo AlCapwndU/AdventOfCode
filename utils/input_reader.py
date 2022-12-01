e2628877477fc3700de2615f2dcbd71c32852bcf
AlCapwndU <alcapwndu@gmail.com>
2022-12-01 11:23:35 -0600

    Updates to ingesting script, to account for unexpect types in inputs.

--- utils/input_reader.py
+++ utils/input_reader.py
@@ -1,5 +1,11 @@
 from collections import defaultdict
 
+def base_type_conversion(value, base_type):
+    try:
+        return base_type(value)
+    except ValueError:
+        return value
+
 # ingest and format data as necessary for functions
 def ingest(filename, file_format, base_type=str, null_value=int):
     """
@@ -17,15 +23,15 @@ def ingest(filename, file_format, base_type=str, null_value=int):
     if file_format == "raw":
         data = open(filename).read()
     elif file_format == "line":
-        data = [line.strip().strip() for line in open(filename).readlines()]
+        data = [base_type_conversion(line.strip().strip(), base_type) for line in open(filename).readlines()]
     elif file_format == "tuple":
         [tuple([col for col in line.strip().split()]) for line in open(filename).readlines()]
     elif file_format == "list":
-        data = [[base_type(val) for val in line.strip().split(",")] for line in open(filename).readlines()]
+        data = [[base_type_conversion(val, base_type) for val in line.strip().split(",")] for line in open(filename).readlines()]
     elif file_format == "grid":
         data = {}
         for j, line in enumerate(open(filename).readlines()):
             for i, value in enumerate(line.strip()):
-                data[(i, j)] = base_type(value)
+                data[(i, j)] = base_type_conversion(value, base_type)
 
     return data
