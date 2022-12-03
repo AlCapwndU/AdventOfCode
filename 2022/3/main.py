07932beb05c5fd5dabca065bc6d050541679009e
AlCapwndU <alcapwndu@gmail.com>
2022-12-03 13:00:49 -0600

    2022 - Day 3

--- 2022/3/main.py
+++ 2022/3/main.py
@@ -2,16 +2,18 @@ from utils.input_reader import ingest
 import part1
 import part2
 
-# format options: raw, line, tuple, list
+# format options: raw, line, tuple, list, grid
 file_format = "line"
 base_type = str
+example_result_1 = 157
+example_result_2 = 70
+
+
 def test_part1():
     # do test on example data
     example_data = ingest("example.input", file_format, base_type=base_type)
 
-    example_result = 24000
-
-    assert part1.run(example_data) == example_result
+    assert part1.run(example_data) == example_result_1
 
     # if test passed, do the real input
     real_data = ingest("real.input", file_format, base_type=base_type)
@@ -20,20 +22,14 @@ def test_part1():
     print(part1.run(real_data))
 
 
-
 def test_part2():
     # do test on example data
     example_data = ingest("example.input", file_format, base_type=base_type)
 
-    example_result = 45000
-
-    assert part2.run(example_data) == example_result
+    assert part2.run(example_data) == example_result_2
 
     # if test passed, do the real input
     real_data = ingest("real.input", file_format, base_type=base_type)
 
     print("\nPart 2 Results\n----------------\n")
     print(part2.run(real_data))
-
-
-
