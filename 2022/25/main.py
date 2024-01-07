d6974d8c6651fd7860960463ab2dffa19d84c67a
AlCapwndU <alcapwndu@gmail.com>
2024-01-07 12:38:34 -0600

    Lots of updates

--- 2022/25/main.py
+++ 2022/25/main.py
@@ -4,23 +4,23 @@ import part2
 
 # format options: raw, line, tuple, list, grid
 file_format = "line"
-base_type = str
-example_result_1 = 26
-example_result_2 = 56000011
+base_type = int
+example_result_1 = None
+example_result_2 = None
 
 
-# def test_part1():
-#     if example_result_1:
-#         # do test on example data
-#         example_data = ingest("example.input", file_format, base_type=base_type)
-#
-#         assert part1.run(example_data) == example_result_1
-#
-#         # if test passed, do the real input
-#         real_data = ingest("real.input", file_format, base_type=base_type)
-#
-#         print("\nPart 1 Results\n----------------\n")
-#         print(part1.run(real_data))
+def test_part1():
+    if example_result_1:
+        # do test on example data
+        example_data = ingest("example.input", file_format, base_type=base_type)
+
+        assert part1.run(example_data) == example_result_1
+
+        # if test passed, do the real input
+        real_data = ingest("real.input", file_format, base_type=base_type)
+
+        print("\nPart 1 Results\n----------------\n")
+        print(part1.run(real_data))
 
 
 def test_part2():
